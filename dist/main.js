49f74e50e332bd07075ab82274e25662093e1a0a
Markus <markus.h.jarvis@outlook.com>
2021-05-01 20:39:04 -0600

    added projects display

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";console.log("Test")})();
+(()=>{"use strict";let t=[],e=new class{constructor(t,e=[]){this.title=t,this.tasks=e}}("Shopping List",["Takek out the Trash"]);t.push(e),function(){for(let e=0;e<t.length;e++)document.getElementsByClassName("project-container"),document.createElement("button"),console.log(t[e].title)}()})();
--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,29 @@
+let projects = [];
+
+class MyProjects {
+    constructor(title, tasks = []) {
+        this.title = title;
+        this.tasks = tasks;
+    }
+}
+
+let test1 = new MyProjects("Shopping List", ["Takek out the Trash"]);
+projects.push(test1);
+
+function displayProjects() {
+    
+    for(let i = 0; i < projects.length; i++) {
+
+        const projectDiv = document.getElementsByClassName("project-container");
+        const button = document.createElement('button');
+        
+        console.log(projects[i].title);
+    }
+}
+
+
 function test() {
-    console.log("Test");
+    displayProjects();
 }
 
 export { test }
