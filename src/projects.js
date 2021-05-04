6f226410c12ef290b1a38fbbbdfdfe78da90201f
Markus <markus.h.jarvis@outlook.com>
2021-05-04 13:44:20 -0600

    Added display function for projects (sidebar)

--- src/projects.js
+++ src/projects.js
@@ -8,14 +8,27 @@ class MyProjects {
 }
 
 let test1 = new MyProjects("Shopping List", ["Takek out the Trash"]);
-projects.push(test1);
+let test2 = new MyProjects("Homework", ["Math", "Spelling"]);
+let test3 = new MyProjects("WorkOut", ["Biceps", "Triceps"]);
 
+projects.push(test1);
+projects.push(test2);
+projects.push(test3);
+
+/* Display individual projects in the side-bar */
 function displayProjects() {
     
     for(let i = 0; i < projects.length; i++) {
 
-        const projectDiv = document.getElementsByClassName("project-container");
+        const projectContainer = document.getElementById("project-container");
+        
+        /* Create a button for each individual project */
         const button = document.createElement('button');
+        let projectName = projects[i].title;
+        button.className = "project-titles";
+        button.id = projectName;
+        button.innerHTML = projectName;
+        projectContainer.appendChild(button);
         
         console.log(projects[i].title);
     }
